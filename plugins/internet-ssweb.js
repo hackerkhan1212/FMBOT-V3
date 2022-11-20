3bb72948ccfda0f22f8b0be4ed30f6615968603f
hackerkhan1212 <94687058+hackerkhan1212@users.noreply.github.com>
2022-11-20 20:50:39 +0500

    Update internet-ssweb.js

--- plugins/internet-ssweb.js
+++ plugins/internet-ssweb.js
@@ -3,26 +3,18 @@ let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 if (!args[0]) throw `Contoh  ${usedPrefix + command} https://s.id`
 
 	let lis = [
-'https://hadi-api.herokuapp.com/api/ssweb?url=' + args[0] + '&device=tablet&full=on',
-'https://hadi-api.herokuapp.com/api/ssweb?url=' + args[0] + '&device=tablet&full=off',
-'https://hadi-api.herokuapp.com/api/ssweb?url=' + args[0] + '&device=desktop&full=on',
-'https://hadi-api.herokuapp.com/api/ssweb?url=' + args[0] + '&device=desktop&full=off',
-'https://hadi-api.herokuapp.com/api/ssweb?url=' + args[0] + '&device=phone&full=on',
-'https://hadi-api.herokuapp.com/api/ssweb?url=' + args[0] + '&device=phone&full=off',
-'https://hadi-api.herokuapp.com/api/ssweb2?url=' + args[0],
-'https://nurutomo.herokuapp.com/api/ssweb?url=' + args[0],
+'https://api.lolhuman.xyz/api/ssweb?apikey=${}&url=args[0]', 
+'https://api.lolhuman.xyz/api/sswebfull?apikey=${}&url= args[0]',
+'https://botcahx-rest-api.herokuapp.com/api/tools/ssweb?link==args[0]', 
+'https://nurutomo.herokuapp.com/api/ssweb?url= args[0]',
 'https://shot.screenshotapi.net/screenshot?token=WCCYKR0-X5CMMV0-JB4G5Z5-P6SPC8R&url=' + args[0] + '&full_page=true&fresh=true&output=image&file_type=jpg',
 'https://api.popcat.xyz/screenshot?url=' + args[0],
 'https://api.apiflash.com/v1/urltoimage?access_key=7eea5c14db5041ecb528f68062a7ab5d&wait_until=page_loaded&url=' + args[0]
 ]
 let liss = [
-'hadi-api tablet full on',
-'hadi-api tablet full off',
-'hadi-api desktop full on',
-'hadi-api desktop full off',
-'hadi-api phone full on',
-'hadi-api phone full off',
-'hadi-api original',
+'Screeshot1',
+'Screenshot full',
+'Screeshot2',
 'nurutomo',
 'shot.screenshotapi',
 'api.popcat',
